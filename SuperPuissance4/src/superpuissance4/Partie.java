4f0e39d4499fd3df3652e9ea92d1733cae827442
utilisateur <utilisateur@192.168.114.140>
2021-11-15 17:16:48 +0100

    fonction attribuer couleur

--- SuperPuissance4/src/superpuissance4/Partie.java
+++ SuperPuissance4/src/superpuissance4/Partie.java
@@ -3,19 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package superpuissance4;
+import java.util.Random;
 
 /**
  *
  * @author richa
  */
 public class Partie {
-    ListeJoueurs = [];
-    joueurCourant;
+    
+    Joueur ListesJoueurs[]=new Joueur[2];
+    Joueur joueurCourant;
     Grille grilleJeu= new Grille();
-    public voir initialiserPartie(){
+    public void initialiserPartie(){
         Cellule[][] CellulesJeu = new Cellule[6][7];
     }
-    
+    public void attribuerCouleursAuxJoueurs(){
+        int c1;
+        int c2;
+       String [] tableauc={"Rouge","Jaune"};
+       Random caleat = new Random();
+       int valaleat = caleat.nextInt(1);
+       c1=valaleat;
+       if (c1==0){
+           
+       }
+       
+       
+       
+       
+        
+        
+    }
     
     
 }
