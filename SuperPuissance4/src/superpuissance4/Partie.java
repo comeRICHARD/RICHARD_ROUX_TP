3c5c03fd39604a47b2d2ad853e8aaf0c947990c4
utilisateur <utilisateur@192.168.114.140>
2021-11-16 23:30:43 +0100

    Partie avancée

--- SuperPuissance4/src/superpuissance4/Partie.java
+++ SuperPuissance4/src/superpuissance4/Partie.java
@@ -4,6 +4,7 @@
  */
 package superpuissance4;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -15,17 +16,55 @@ public class Partie {
     Joueur joueurCourant;
     Grille grilleJeu= new Grille();
     public void initialiserPartie(){
+        //On commence par vider la grille
+        grilleJeu.viderGrille();
         Cellule[][] CellulesJeu = new Cellule[6][7];
+        //Entrée des noms
+        Scanner sc = new Scanner(System.in);
+        String nom_j1;
+        String nom_j2;
+       
+        System.out.println("Joueur 1: Entrez votre nom");
+        nom_j1=sc.nextLine();
+        System.out.println("Joueur 2: Entrez votre nom");
+        nom_j2=sc.nextLine();
+       
+        Joueur joueur_1= new Joueur(nom_j1);
+        Joueur joueur_2=new Joueur (nom_j2);
+        //Attribution des couleurs
+        attribuerCouleursAuxJoueurs();
+        // Attribution des Jetons
+                // On donne des jetons aux joueurs
+        for (int i = 0; i < 21; i++) {
+
+            Jeton j = new Jeton(Listejoueurs[0].Couleur);
+
+            joueur_1.ajouterJeton(j);
+
+            joueur2.ajouterJeton(new Jeton(joueur_2.Couleur));
+        }
+
+
+        
+                
+           
+        
     }
     public void attribuerCouleursAuxJoueurs(){
-        int c1;
-        int c2;
+        int n1;
+       String c1;
+       String c2;
        String [] tableauc={"Rouge","Jaune"};
        Random caleat = new Random();
        int valaleat = caleat.nextInt(1);
-       c1=valaleat;
-       if (c1==0){
+       n1=valaleat;
+       if (n1==0){
+          Listesjoueurs[0]=new affecterCouleur(tableauc[n1]);
+          Listesjoueurs[1].affecterCouleur()=tableauc[1];
            
+       }else{
+          Listesjoueurs[0].Couleur =tableauc[n1];
+          Listesjoueurs[1].Couleur=tableauc[0];
        }
        
        
