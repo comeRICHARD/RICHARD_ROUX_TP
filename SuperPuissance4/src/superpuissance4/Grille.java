85e29ea45fe5b1be75030f123fdf67e8f1c59fc3
utilisateur <utilisateur@192.168.114.140>
2021-11-27 23:02:39 +0100

    jeu semi fonctionnel

--- SuperPuissance4/src/superpuissance4/Grille.java
+++ SuperPuissance4/src/superpuissance4/Grille.java
@@ -9,237 +9,261 @@ package superpuissance4;
  *
  * @author richa
  */
-public class Grille {
-    Cellule[][] CellulesJeu = new Cellule[6][7];
-   
-   
-
-   
-    public  Grille(){
-      for(int i=0 ; i<6 ; i++){
-          for(int j=0 ; j<7 ; j++){
-              CellulesJeu[i][j] = new Cellule();
-          }
-      }
-    }
-    public boolean etreRemplie(){
-        for (int i=0 ; i<6 ; i++ ){
-            for (int j=0 ; j<7 ; j++){
-                if (CellulesJeu[i][j].jetonCourant==null){
-                    return false;
-               
-                   
-                }
-               
-            }
-        }
-        return true;
-    }
-    public boolean Colonneremplie(int num_colonne){
-        for (int i =0 ; i<6 ; i++){
-            if(CellulesJeu[i][num_colonne]!=null){
+public class Grille
+{
+    public static final int NOMBRE_DE_LIGNES = 6;
+    public static final int NOMBRE_DE_COLONNES = 7;
+    
+    // cellules serait un meilleur nom
+    private Cellule[][] cellulesJeu = new Cellule[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES]; // variables commencent toujours par une minuscule
+	
+	public Grille()
+	{
+		for (int i = 0; i < NOMBRE_DE_LIGNES; i++)
+		{
+			for (int j = 0; j < NOMBRE_DE_COLONNES; j++)
+			{
+				cellulesJeu[i][j] = new Cellule();
+			}
+		}
+	}
+	
+	public boolean etreRemplie()
+	{
+		for (int i = 0; i < NOMBRE_DE_LIGNES; i++)
+		{
+			for (int j = 0; j < NOMBRE_DE_COLONNES; j++)
+			{
+				if (cellulesJeu[i][j].jetonCourant == null)
+				{
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	public boolean Colonneremplie(int numColonne){
+	
+		if (cellulesJeu[5][numColonne].jetonCourant!=null){
                 return true;
-           
+            }else{
+                return false;
             }
-        }
-        return false;
-    }
-
-    public boolean ajouterJetonDansColonne(Jeton j,int num_colonne){
-        int k = 0;
-           if (Colonneremplie(num_colonne)==true){
-               return false;
-           }
-           
-           while (CellulesJeu[k][num_colonne].jetonCourant != null) {
-            k++;
-           }
-           CellulesJeu[k][num_colonne].affecterJeton(j);
-           
-            if (CellulesJeu[k][num_colonne].Desintegrateur()) {
-            CellulesJeu[k][num_colonne].recupererDesintegrateur();
-            //jetonCourant.nombreDesintegrateurs++;
-            }
-           
-             
-            if (CellulesJeu[k][num_colonne].presenceTrouNoir()) {
-            CellulesJeu[k][num_colonne].activerTrouNoir();
-            }
-       
-                       
-               
-       
+	}
+	
+	public boolean ajouterJetonDansColonne(Jeton j, int num_colonne) // c'est mieux de nommer numColonne
+	{
+		int k = 0;
+		if (Colonneremplie(num_colonne)) 
+		{
+			return false;
+		}
+		
+		while (cellulesJeu[k][num_colonne].jetonCourant != null)
+		{
+			k++;
+		}
+		cellulesJeu[k][num_colonne].affecterJeton(j);
+		
+		if (cellulesJeu[k][num_colonne].Desintegrateur())
+		{
+			cellulesJeu[k][num_colonne].recupererDesintegrateur();
+			//jetonCourant.nombreDesintegrateurs++;
+		}
+		
+		if (cellulesJeu[k][num_colonne].presenceTrouNoir())
+		{
+			cellulesJeu[k][num_colonne].activerTrouNoir();
+		}
+		
+		return true;
+	}
+	
+	public void viderGrille()
+	{
+		for (int i = 0; i < NOMBRE_DE_LIGNES; i++)
+		{
+			for (int j = 0; j < NOMBRE_DE_COLONNES; j++)
+			{
+				cellulesJeu[i][j].jetonCourant = null;
+			}
+		}
+	}
+	
+	public void afficherGrilleSurConsole()
+	{
+		for (int i = 5; i >= 0; i--)
+		{
+			System.out.println();
+			for (int j = 0; j < NOMBRE_DE_COLONNES; j++)
+			{
+				
+				if (celluleOccupee(i, j))
+				{// Cas ou il y a un jeton dans la cellule
+					if (lireCouleurDuJeton(i, j) == "Rouge")
+					{
+						System.out.print("\u001B[31m  0  ");
+                                                //On print un O rouge
+					}
+					else
+					{
+						System.out.print("\u001B[33m  0  ");
+                                                //On print un O en jaune
+					}
+					
+				}
+				else if (cellulesJeu[i][j].Desintegrateur())
+				{// Si il y a la présence d'un désintégrateur
+					if (cellulesJeu[i][j].presenceTrouNoir())
+					{
+						System.out.print("\u001B[0m | T | "); //On mets le trou noir devant le désintegrateur 
+					}
+					else
+					{
+						System.out.print("\u001B[0m | D | ");// On affiche le désintégrateurs
+					}
+				}
+				
+				else if (cellulesJeu[i][j].presenceTrouNoir() )
+				{
+					System.out.print("\u001B[0m | T | ");
+				}
+				else
+				{
+					System.out.print("| _ |");
+				}
+			}
+		}
+	}
+	
+	public boolean celluleOccupee(int num_ligne, int num_colone)
+	{
+		return cellulesJeu[num_ligne][num_colone].jetonCourant != null;
+	}
+	
+	public String lireCouleurDuJeton(int num_ligne, int num_colone)
+	{
+		return cellulesJeu[num_ligne][num_colone].lireCouleurDuJeton();
+	}
+	
+	public boolean etreGagnantePourJoueur(Joueur joueur)
+	{
+		String Cj = joueur.Couleur; // lower case pour la premiere lettre de la variable
+		
+		//Verifiaction des lignes
+		for (int i = 0; i <= 5; i++)
+		{
+			for (int j = 0; j <= 3; j++)
+			{
+			    // source d'erreur. tu utilises == pour comparer des string alors qu'il faut utiliser stringA.equals(stringB)
+			    
+				if (cellulesJeu[i][j] != null && cellulesJeu[i][j].lireCouleurDuJeton().equals(Cj)
+					&& cellulesJeu[i + 1][j + 1] != null && cellulesJeu[i][j + 1].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i + 2][j + 2] != null && cellulesJeu[i][j + 2].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i + 3][j + 3] != null && cellulesJeu[i][j + 3].lireCouleurDuJeton() == Cj)
+				{
+					return true;
+				}
+				
+			}
+			
+		}
+		//Verification des colonnes
+		for (int i = 0; i <= 2; i++)
+			for (int j = 0; j <= 6; j++)
+			{
+				if (cellulesJeu[i][j] != null && cellulesJeu[i][j].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i][j + 1] != null && cellulesJeu[i][j + 1].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i][j + 2] != null && cellulesJeu[i][j + 2].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i][j + 3] != null && cellulesJeu[i][j + 3].lireCouleurDuJeton() == Cj)
+				{
+					return true;
+				}
+				
+			}
+		
+		//Verification des diagonales vers le haut
+		for (int i = 0; i <= 2; i++)
+		{
+			for (int j = 0; j <= 3; j++)
+			{
+				if (cellulesJeu[i][j] != null && cellulesJeu[i][j].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i][j + 1] != null && cellulesJeu[i][j + 1].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i][j + 2] != null && cellulesJeu[i][j + 2].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i][j + 3] != null && cellulesJeu[i][j + 3].lireCouleurDuJeton() == Cj)
+				{
+					return true;
+				}
+				
+			}
+		}
+		
+		//Verification des diagonales vers le bas
+		for (int i = 3; i <= 5; i++)
+		{
+			for (int j = 0; j <= 3; j++)
+			{
+				if (cellulesJeu[i][j] != null && cellulesJeu[i][j].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i][j + 1] != null && cellulesJeu[i][j + 1].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i][j + 2] != null && cellulesJeu[i][j + 2].lireCouleurDuJeton() == Cj
+					&& cellulesJeu[i][j + 3] != null && cellulesJeu[i][j + 3].lireCouleurDuJeton() == Cj)
+				{
+					return true;
+				}
+			}
+			
+		}
+		return false;
+	}
+	
+	public Boolean supprimerJeton(int num_ligne, int num_colonne)
+	{
+		if (cellulesJeu[num_ligne][num_colonne].jetonCourant == null)
+		{
+		   
+			return false;
+		}
+        
+        cellulesJeu[num_ligne][num_colonne].jetonCourant = null;
         return true;
+	}
+	
+	public boolean colonneRemplie(int num_colonne)
+	{
+	    // tout ca peut etre remplace par 
+        // return cellulesJeu[5][num_colonne].jetonCourant != null
+		
+		if (cellulesJeu[5][num_colonne].jetonCourant == null)
+			return false;
+		
+		return true;
+	}
+	
+	public void tasserGrille(int num_colonne)
+	{
+		for (int i = 0; i < NOMBRE_DE_COLONNES; i++)
+		{
+			for (int j = 0; j < NOMBRE_DE_LIGNES; i++)
+			{
+				if (i == 5)
+				{
+					cellulesJeu[i][num_colonne].jetonCourant = null;
+				}
+				else
+				{
+					if (cellulesJeu[i][num_colonne].jetonCourant == null)
+					{
+						cellulesJeu[i][num_colonne].jetonCourant = cellulesJeu[i + 1][num_colonne].jetonCourant;
+						cellulesJeu[i + 1][num_colonne].jetonCourant = null;
+					}
+				}
+			}
+		}
+	}
 }
-   
-         
-   
-   
-    public void viderGrille(){
-        for (int i=0 ; i<6 ; i++ ){
-            for (int j=0 ; j<7 ; j++){
-                CellulesJeu[i][j].jetonCourant=null;
-            }
-        }
-    }
-    public void afficherGrilleSurConsole(){
-        for(int i=5;i>=0;i--){
-            System.out.println(); 
-            for(int j=0;j<7;j++){
-                
-                if (celluleOccupee(i,j)==true){// Cas ou il y a un jeton dans la cellule
-                      if (lireCouleurDuJeton(i,j)=="Rouge") {
-
-                    System.out.print("\u001B[31m  0  "); //On print un O rouge
-
-                }else {
-
-                   System.out.print("\u001B[33m  0  "); //On print un O en jaune
-
-                
-
-                }
-                    
-                }
-                else if (CellulesJeu[i][j].Desintegrateur()==true){// Si il y a la présence d'un désintégrateur
-                    if (CellulesJeu[i][j].presenceTrouNoir()==true){
-                        System.out.print("\u001B[0m | T | "); //On mets le trou noir devant le désintegrateur 
-                    }else{
-                         System.out.print("\u001B[0m | D | ");// On affiche le désintégrateurs
-                    }
-                }
-                    
-                  
-                else if(CellulesJeu[i][j].presenceTrouNoir()==true){
-                      System.out.print("\u001B[0m | T | ");
-                            
-                    
-                }else{
-                       System.out.print("| _ |");
-                       }                  
-                
-                
-                  
-
-                
-                }
-               
-        }
-    }
-    
-    
-   
-    public boolean celluleOccupee(int num_ligne, int num_colone){
-        if(CellulesJeu[num_ligne][num_colone]!=null){
-            return true;
-        }else{
-            return false;
-        }
        
-    }
-    public String lireCouleurDuJeton(int num_ligne, int num_colone){
-        return CellulesJeu[num_ligne][num_colone].lireCouleurDuJeton();
-       
-       
-    }
-    public boolean etreGagnantePourJoueur(Joueur joueur ){
-      String Cj=joueur.Couleur;
-       
-        //Verifiaction des lignes
-        for (int i=0;i<=5;i++){
-            for (int j=0;j<=3;j++){
-                if(CellulesJeu[i][j]!=null && CellulesJeu[i][j].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i+1][j+1]!=null && CellulesJeu[i][j+1].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i+2][j+2]!=null &&CellulesJeu[i][j+2].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i+3][j+3]!=null && CellulesJeu[i][j+3].lireCouleurDuJeton()==Cj){
-                    return true;
-                }
-               
-               
-            }
-           
-           
-        }
-        //Verification des colonnes
-        for(int i=0;i<=2;i++)
-            for (int j=0;j<=6;j++){
-                if(CellulesJeu[i][j]!=null && CellulesJeu[i][j].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i][j+1]!=null && CellulesJeu[i][j+1].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i][j+2]!=null && CellulesJeu[i][j+2].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i][j+3]!=null && CellulesJeu[i][j+3].lireCouleurDuJeton()==Cj){
-                    return true;
-                }
-               
-            }
-       
-        //Verification des diagonales vers le haut
-        for(int i=0;i<=2;i++){
-            for (int j=0;j<=3;j++){
-                if(CellulesJeu[i][j]!=null && CellulesJeu[i][j].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i][j+1]!=null && CellulesJeu[i][j+1].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i][j+2]!=null && CellulesJeu[i][j+2].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i][j+3]!=null && CellulesJeu[i][j+3].lireCouleurDuJeton()==Cj){
-                    return true;
-                }
-               
-               
-            }
-        }
-       
-        //Verification des diagonales vers le bas
-        for(int i=3;i<=5;i++){
-            for (int j=0;j<=3;j++){
-                if(CellulesJeu[i][j]!=null && CellulesJeu[i][j].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i][j+1]!=null && CellulesJeu[i][j+1].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i][j+2]!=null && CellulesJeu[i][j+2].lireCouleurDuJeton()==Cj
-                        && CellulesJeu[i][j+3]!=null && CellulesJeu[i][j+3].lireCouleurDuJeton()==Cj){
-                    return true;
-                }
-            }
-               
-            }
-        return false;
-    }
-       public Boolean supprimerJeton(int num_ligne,int num_colonne){
-           if(CellulesJeu[num_ligne][num_colonne].jetonCourant==null ){
-               return false;
-       }else{
-                CellulesJeu[num_ligne][num_colonne].jetonCourant=null;
-                return true;}
-               
-               
-   
-           
-       }
-       public boolean colonneRemplie(int num_colonne){
-           
-               if (CellulesJeu[5][num_colonne].jetonCourant==null){
-                   return false;
-                   
-               }else{
-           
-           return true;
- 
-       }
-       }
-
-
-
-       public void tasserGrille(int num_colonne){
-           for (int i=0 ; i<7 ; i++){
-               for(int j=0 ; j<6 ; i++){
-                   if (i == 5) {
-                CellulesJeu[i][num_colonne].jetonCourant = null;
-            } else {
-                if (CellulesJeu[i][num_colonne].jetonCourant  == null) {
-                  CellulesJeu[i][num_colonne].jetonCourant = CellulesJeu[i + 1][num_colonne].jetonCourant;
-                  CellulesJeu[i + 1][num_colonne].jetonCourant=null;
-                }
-            }
-               }
-           }
-       }
-}
        
            
         
--- SuperPuissance4/src/superpuissance4/Joueur.java
+++ SuperPuissance4/src/superpuissance4/Joueur.java
@@ -9,42 +9,66 @@ package superpuissance4;
  *
  * @author richa
  */
-public class Joueur {
-    String nom;
-    String Couleur;
-    Jeton ListeJetons []=new Jeton[21];
-    int nombreDesintegrateurs;
-    int nombreJetonsRestants;
-public Joueur(String unnom){
-    nom=unnom;
-   
-}    
-
-public void affecterCouleur(String unecouleur){
-    Couleur=unecouleur;
-}
-public boolean ajouterJeton(Jeton  j ){
-   for(int i=0;i<ListeJetons.length;i++){
-       if (ListeJetons[i]==null){
-           ListeJetons[i]=j;
-           return true;  
-   }
-       
-}
-   return false;
-}
-
-public void ObtenirDesintegrateurs(){
-    nombreDesintegrateurs+=1;
-}
-public boolean utiliserDesintegrateur(){
-    if (nombreDesintegrateurs!=0){
-        nombreDesintegrateurs-=1;
-        return true;
-    }
-    else{
-      return false;  
-    }
-     
-}
+public class Joueur
+{
+    // mets tout en private avec des getter/setter
+    
+	String nom;
+	String Couleur;
+	
+	// ce serait mieux d'utiliser une ArrayList. Comme ca tu peux faire add et remove dessus
+    // comme ca tu peux virer nombreJetonsRestants
+    // List<Jeton> jetons = new ArrayList<>();
+    // Jeton remove = jetons.remove(0);
+    // jetons.size()
+    
+	Jeton  ListeJetons[] = new Jeton[21]; // jetons serait un meilleur nom 
+	int    nombreDesintegrateurs;
+	int    nombreJetonsRestants;
+	
+	public Joueur(String unnom)
+	{
+		nom = unnom;
+	}
+	
+	// cette methode n'est jamais utilisee
+	public void affecterCouleur(String unecouleur)
+	{
+		Couleur = unecouleur;
+	}
+	
+	// j est reserve pour nommer les variables dans un boucle for. Renomme le en jeton
+	public boolean ajouterJeton(Jeton jeton)
+	{
+	    // avec une liste, tout ce que tu as à faire ici est 
+        // jetons.add(j)
+        // de plus la methode pourrait etre void plutot que retourner boolean
+	    
+		for (int i = 0; i < ListeJetons.length; i++)
+		{
+			if (ListeJetons[i] == null)
+			{
+				ListeJetons[i] = jeton;
+				return true;
+			}
+			
+		}
+		return false;
+	}
+	
+	public void ObtenirDesintegrateurs()
+	{
+		nombreDesintegrateurs += 1;
+	}
+	
+	public boolean utiliserDesintegrateur()
+	{
+		if (nombreDesintegrateurs != 0)
+		{
+			nombreDesintegrateurs -= 1;
+			return true;
+		}
+        
+        return false;
+	}
 }  
