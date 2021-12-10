fcfc30fbdbd8315660fdaa724f1d124aafdd7354
richa <richa@LAPTOP-0BGMRRFG>
2021-12-10 08:42:29 +0100

    methode initialiser partie + colonneremplie + lirechiffre

--- Bloqué/src/bloqué/Grille.java
+++ Bloqué/src/bloqué/Grille.java
@@ -3,40 +3,84 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package bloqué;
+import java.util.Random;
 
 /**
  *
- * @author richa
+ * @author utilisateur
  */
 public class Grille {
-     int grille[][] = new int[4][4];
-    public boolean GrilleRemplie(){
-        
-        
-        
-        return false;
-}
-    public void AfficherGrille(){
-        System.out.println();
-    }
-    public int LireChiffre(int i ,int j){
-        return grille[i][j];
-    }
-    public boolean ColonneRemplie(int num_cln){
-        for (int i=0 ; i<4 ; i++){
-        if (grille[i][num_cln]==0){
-            return false;}
-        else {
-            
-        
-        
-         return true;
-        }
-        
-    }
-        
-    }
-
+    Carte cartecourante;
+    int[] tabcarte={1,2,3};
     
+   public Integer[][]grille=new Integer[4][4];
+    
+    public Grille(){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                grille[i][j]=null;
+                
+            }
+        }
+    }
+    public boolean etreremplie(){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                if (grille[i][j]!=null){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean colonnepleine(int numcolonne){
+        for(int i=0;i<4;i++){
+            if(grille[i][numcolonne]!=null){
+                return false;
+                
+            }
+        }
+        return true;
+    }
+    public boolean ligneplein(int numligne){
+        for(int j=0;j<4;j++){
+            if(grille[numligne][j]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean ajoutercartedanscolonne(int numligne,int numcolonne){
+        Random t=new Random();
+        int tirage=t.nextInt(3);  
+        int numeroCarte=tabcarte[tirage];
+        if(grille[numligne][numcolonne]==null){
+            grille[numligne][numcolonne]=numeroCarte;
+            return true;
+        }
+        return false;
+        
+        
+        
+    }
+    public void afficherlagrillesurconsole(){
+        
+    }
+    public void additionnercartes(String direction){
+        
+    }
+    public void tassergrille(){
+        
+    }
+    public int aditionnergrille(){
+        int score=0;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                score+=grille[i][j];
+            }
+        }
+        return score;
+    }
 }
-   
+
+
